db9227eefd34e58d1b8e27b6bbacf57b788fbcc1
pranavjosephtossy <b00158966@mytudublin.ie>
2026-05-03 12:58:10 +0100

    a

--- Code/pipeline.py
+++ Code/pipeline.py
@@ -1,20 +1,19 @@
 from Storage import evidence_storage
+from analysis import *
 
-json_dump = [
-    {
-    "content": "This is an example",
-    "username": "example_adbd",
-    "subreddit": "cybersecurity",
-    "timestamp": "2026-04-5"
-    },
-    {
-    "content": "This is also an example"
-    }
-]
 def run_main():
-    for raw_item in json_dump:
+        #calls collection module and gets back raw_json and log item
+        collection()
 
-        # calls collection and gets back raw_json and log item
-        # calls analysis passes raw_json and gets analysis_json and log item
-        # calls storage passes both jsons and logs
-        evidence_storage()
+        with open("youtube_collection_with_comments.json","r") as f:
+              json_dump = json.load(f)
+
+        for raw_item in json_dump:
+            #i need a way to log each raw item has been collected
+            raw_json, collection_log = collect_item(raw_item)
+
+            #calls analysis passes raw_json and gets analysis_json and log item
+            analysis_json, analysis_log = analysis(raw_json)
+            
+            # calls storage passes both jsons and logs
+            evidence_storage(raw_json, analysis_json, collection_log, analysis_log)
