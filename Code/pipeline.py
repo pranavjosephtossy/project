4647edf93a592282d6cd8cf8bb78e7ccf43da5f2
pranavjosephtossy <b00158966@mytudublin.ie>
2026-05-09 12:34:38 +0100

    Update pipeline.py

--- Code/pipeline.py
+++ Code/pipeline.py
@@ -1,23 +1,22 @@
 from Storage import evidence_storage
 from main import analysis
-import json
-from collect_item import collect_item
 
 def run_main():
         
       user_input=input("1 for synthetic,2 for preinstalled yt and 3 for custom")
-      if user_input== "1":
-            print("Not implemented yet")
+      if user_input==1:
+            with open("Offensive_content_reddit","r") as f:
+                  json_dump = json.load(f)
 
-      elif user_input=="2":
-            with open("Offensive_content_reddit.json","r", encoding="utf-8") as f:
+      elif user_input==2:
+            with open("youtube_collection_with_comments.json","r") as f:
                   json_dump = json.load(f)
             
-      elif user_input=="3":
+      elif user_input==3:
             user_input_yt_title=input("Enter yt link")
             #calls collection module and gets back raw_json and log item
             collection()
-            with open("youtube_collection_with_comments.json","r", encoding="utf-8") as f:
+            with open("youtube_collection_with_comments.json","r") as f:
                   json_dump = json.load(f)
 
       else:
@@ -35,8 +34,9 @@ def run_main():
                   # calls storage passes both jsons and logs
                   forensic_package=evidence_storage(comment, analysis_json, collection_log, analysis_log)
 
-                  output_file = f"forensic__package{raw_item['video_id']}.json"
-                  print(json.dumps(forensic_package, indent=4))   # dumps with an "s" returns a string
-                  with open(output_file, "w", encoding="utf-8") as outfile:
-                        json.dump(forensic_package, outfile, indent=4)   # outfile, not output_file
+            print("Forensic Package")
+            print(json.dump(forensic_package, output_file, indent=4))
+            output_file = f"forensic__package{raw_json['video_id']}.json"
+            with open(output_file, "w", encoding="utf-8") as outfile:
+                  json.dump(forensic_package, output_file, indent=4)
 run_main()
